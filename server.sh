739e729515430b92e8d4143936523fe907eb6d36
Yuri <jurasiq@gmail.com>
2025-12-24 00:49:55 +0300

    energybyny v5

---
+++ server.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Start a simple HTTP server on port 8000 (macOS/Linux)
+python3 -m http.server 8000
---
+++ server_mac.sh
@@ -0,0 +1,2 @@
+echo 'alias rive-server="cd /Users/yuri/Library/CloudStorage/Dropbox/Settings/Rive && python3 -m http.server 8000"' >> ~/.zshrc
+source ~/.zshrc
