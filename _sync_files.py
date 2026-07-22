e9a998902cca8a28e7410d53ccf1b7044ba6bb56
Yuri <jurasiq@gmail.com>
2026-07-22 03:15:00 +0100

    animations update

--- _sync_files.py
+++ _sync_files.py
@@ -20,7 +20,8 @@ with CSV_FILE.open(newline="") as file:
 OLD_DIR.mkdir(exist_ok=True)
 
 for riv_file in RIV_DIR.glob("*.riv"):
-	if riv_file.stem not in active_names:
+	active_name = riv_file.stem.removesuffix("_preview")
+	if active_name not in active_names:
 		shutil.move(str(riv_file), OLD_DIR / riv_file.name)
 
 shutil.copy2(CSV_FILE, PROJECT_DIR / CSV_FILE.name)
